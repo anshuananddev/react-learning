d977d6d983613da4d4c93e2664c0a5f9a9c3cb42
anshuanand <anshu.anand@aa.com>
2025-11-07 15:23:02 +0530

    Nested elements via React

--- App.js
+++ App.js
@@ -4,6 +4,12 @@ const heading = React.createElement(
         id: "heading", 
         xyz: "something random" // custom attribute can also be provide 
     },
-    "Hello, world from React!");
+    "Hello, world from React!"
+);
+const child = React.createElement("div", { id: "child" }, heading);
+const parent = React.createElement("div", { id: "parent" }, child);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+console.log(parent);
+root.render(parent);
