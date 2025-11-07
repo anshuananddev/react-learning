a74abaeeadd697883ea5c313b49d12a2b88766b8
anshuanand <anshu.anand@aa.com>
2025-11-07 15:10:28 +0530

    custom attribute in create element

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 const heading = React.createElement(
     "h1",
     {
-        id: "heading"
+        id: "heading", 
+        xyz: "something random" // custom attribute can also be provide 
     },
     "Hello, world from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
