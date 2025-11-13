6f38d2ec505fa7de0f5d152cdefbef886b675f47
anshuanand <anshuanand21.07@gmail.com>
2025-11-13 18:52:59 +0530

    JSX introduction

--- App.js
+++ App.js
@@ -1,27 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-    "h1",
-    {
-        id: "heading", 
-        xyz: "something random", // custom attribute can also be provide
-        key: "uniqueKey1" 
-    },
-    "Hello, world from React!"
-);
 
-const smallerHeading = React.createElement(
-    "h3",
-    {
-        id: "smallerHeading",
-        key: "uniqueKey2"
-    },
-    "This is a smaller heading" 
-);
-const child = React.createElement("div", { id: "child" }, [heading, smallerHeading]);
-const parent = React.createElement("div", { id: "parent" }, child);
+
+
+// JSX -> HTML/XML like syntax
+const heading = <h1 id = "heading">Namaste React</h1> ; 
+
+
+
+
+// const heading = React.createElement(
+//     "h1",
+//     {
+//         id: "heading", 
+//         xyz: "something random", // custom attribute can also be provide
+//         key: "uniqueKey1" 
+//     },
+//     "Hello, world from React!"
+// );
+
+// const smallerHeading = React.createElement(
+//     "h3",
+//     {
+//         id: "smallerHeading",
+//         key: "uniqueKey2"
+//     },
+//     "This is a smaller heading" 
+// );
+// const child = React.createElement("div", { id: "child" }, [heading, smallerHeading]);
+// const parent = React.createElement("div", { id: "parent" }, child);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(parent);
-root.render(parent);
+console.log(heading);
+root.render(heading);
