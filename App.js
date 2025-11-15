2952b1def13fea53306a9ec882a921e7e0c213ba
anshuanand <anshuanand21.07@gmail.com>
2025-11-15 13:24:46 +0530

    Nested Componenets

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-
 // JSX -> HTML/XML like syntax
 // Babel Transpiler -> Transpile JSX to React.createElement() calls - present as part of parcel bundler
-const heading = (<h1 id = "heading" 
-    className="headingClass">
-        Namaste React 🚗</h1> )
-        ; 
+const heading = (
+  <h1 id="heading" className="headingClass">
+    Namaste React 🚗
+  </h1>
+);
+// React Functional Component
 
-        // React Functional Component
+const Title = () => <h1> This is a title from a component </h1>;
 const HeadingComponent = () => {
-    return (
-        <h1> This is a heading from a component </h1>
-    ) ;
-}
+  return (
+    <>
+      <Title />
+      <h1> This is a heading from a component </h1>
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
